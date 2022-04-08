2f828f6261b6add3b8c6056864ba400482b9df0e
Ypq5701z <volkovkonstantinn@gmail.com>
2022-04-08 10:03:53 +0300

    final

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for (Figures temporaryFigure: Figures.figures) {
+            temporaryFigure.output();
+        }
+    }
+}
